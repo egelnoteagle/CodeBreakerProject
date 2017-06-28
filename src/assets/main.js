f77b9153c264dcf20c7106e1d3011e0cd42ffeb0
Derek Egel <degel@cars.com>
2017-06-28 16:00:41 -0500

    finished 6th task

--- src/assets/main.js
+++ src/assets/main.js
@@ -24,3 +24,11 @@ function setMessage(message) {
   document.getElementById('message').innerHTML = message;
 }
 
+function validateInput(input) {
+  if(input.length != 4) {
+    setMessage('Guesses must be exactly 4 characters long.');
+    return false;
+  }
+  return true;
+}
+
