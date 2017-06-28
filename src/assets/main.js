869d05b23798c5527e1a8bd4e7d8ba853e06f94a
Derek Egel <degel@cars.com>
2017-06-28 15:40:23 -0500

    Finished first 3 tasks

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,3 +7,13 @@ function guess() {
 }
 
 //implement new functions here
+
+function setHiddenFields() {
+  answer.value = Math.floor(Math.random() * 10000).toString();
+  while(answer.value.length < 4) {
+    answer.value = "0" + answer.value;
+  }
+
+  attempt.value = "0";
+}
+
